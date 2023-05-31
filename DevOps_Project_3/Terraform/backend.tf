4e24eddf1168eab6d0ea7bd7d58a084eb656b323
cmamith <49636234+cmamith@users.noreply.github.com>
2023-05-31 15:50:12 +0800

    Update backend.tf

--- DevOps_Project_3/Terraform/backend.tf
+++ DevOps_Project_3/Terraform/backend.tf
@@ -4,6 +4,6 @@ terraform {
   backend "s3" {
     bucket = "euran-terraform-statefile2"
     key = "eks/ngg_cluster_name/statefile"
-    region = "ap-northeast-1"
+    region = "ap-south-1"
   }
 } 
